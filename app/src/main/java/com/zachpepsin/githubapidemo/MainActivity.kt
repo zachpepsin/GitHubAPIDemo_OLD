5a3fbd53017419a1a7021b7c87374ba9b04b8f86
Zach Pepsin <zachpepsin@gmail.com>
2019-10-17 18:18:12 -0400

    Created RecyclerView and adapter

--- app/src/main/java/com/zachpepsin/githubapidemo/MainActivity.kt
+++ app/src/main/java/com/zachpepsin/githubapidemo/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.zachpepsin.githubapidemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewManager = LinearLayoutManager(this)
+
+        val tempDataset = arrayOf("One", "Two", "Three")
+        viewAdapter = RecyclerAdapter(tempDataset)
+
+        recyclerView = findViewById<RecyclerView>(R.id.recycler_main).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = viewManager
+
+            // specify an viewAdapter (see also next example)
+            adapter = viewAdapter
+
+        }
     }
 }
--- app/src/main/java/com/zachpepsin/githubapidemo/RecyclerAdapter.kt
+++ app/src/main/java/com/zachpepsin/githubapidemo/RecyclerAdapter.kt
@@ -1,4 +1,45 @@
 package com.zachpepsin.githubapidemo
 
-class RecyclerAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_recycler.view.*
+
+class RecyclerAdapter(private val myDataset: Array<String>) :
+    RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder.
+    // Each data item is just a string in this case that is shown in a TextView.
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        // Holds the TextView that will set for each item
+        val textHeader = view.text_header
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        // create a new view
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_recycler, parent, false) as View
+        // set the view's size, margins, paddings and layout parameters
+        // ...
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.textHeader.text = myDataset[position]
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = myDataset.size
+
 }
